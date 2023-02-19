4076d88b55b4c83911aa0c672e082abf8c554150
0xBanana <0xbanana@Adils-MacBook-Pro.local>
2023-02-18 17:55:26 -0800

    fix: update export

--- src/components/elements/Card/CardBio.tsx
+++ src/components/elements/Card/CardBio.tsx
@@ -7,15 +7,15 @@ export interface CardBioProps {
 
 const CardBio: FC<CardBioProps> = ({ bio, title }) => {
   return (
-    <div class="nm-flat-gray-200-lg rounded-lg p-8 text-center max-w-sm w-full">
-      <h2 class="text-2xl font-bold leading-tight mb-4">{title}</h2>
+    <div className="nm-flat-gray-200-lg rounded-lg p-8 text-center max-w-sm w-full">
+      <h2 className="text-2xl font-bold leading-tight mb-4">{title}</h2>
       <p>
         Tourmaline is a gemstone that is akin to granite in structure and is acknowledged for its magical powers. This gemstone
         has a unique quality of creating an amount of electrical charge and can give out negative charge.
       </p>
       <a
         href="#"
-        class="rounded-full bg-green-500 shadow-gray-200 leading-5 px-8 py-4 uppercase font-bold tracking-widest text-white inline-block mt-4"
+        className="rounded-full bg-green-500 shadow-gray-200 leading-5 px-8 py-4 uppercase font-bold tracking-widest text-white inline-block mt-4"
       >
         Find out more
       </a>
